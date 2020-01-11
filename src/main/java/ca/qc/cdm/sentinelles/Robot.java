bf73943a50db9880a7a05468c08963bcff881b01
altus34 <altus34@gmail.com>
2020-01-11 13:29:35 -0500

    WIP

--- src/main/java/ca/qc/cdm/sentinelles/Robot.java
+++ src/main/java/ca/qc/cdm/sentinelles/Robot.java
@@ -40,7 +40,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void autonomousInit() {
-        robotContainer.getAutonomousCommand().schedule();
+//        robotContainer.getAutonomousCommand().schedule();
     }
 
     @Override
--- src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
+++ src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
@@ -1,4 +1,14 @@
 package ca.qc.cdm.sentinelles;
 
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
 public class RobotContainer {
+    final Subsystem aa = new Subsystem() {
+
+    };
+
+
+
+
+
 }
