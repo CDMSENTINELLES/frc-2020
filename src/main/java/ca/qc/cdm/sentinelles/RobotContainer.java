ebbf81c5a8d8bc3a6de75c3ccbf7a32ee5146883
altus34 <altus34@gmail.com>
2020-01-20 18:22:05 -0500

    WIP

--- src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
+++ src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
@@ -1,19 +1,15 @@
 package ca.qc.cdm.sentinelles;
 
-import ca.qc.cdm.sentinelles.Constants.OIConstants;
 import ca.qc.cdm.sentinelles.commands.NullCommand;
 import ca.qc.cdm.sentinelles.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
     private final DriveSubsystem drive = new DriveSubsystem();
     Joystick joystick = new Joystick(1);
-//    private XboxController xboxController = new XboxController(OIConstants.kDriverControllerPort);
 
     private final Command autoCommand = new NullCommand();
 
--- src/main/java/ca/qc/cdm/sentinelles/subsystems/DriveSubsystem.java
+++ src/main/java/ca/qc/cdm/sentinelles/subsystems/DriveSubsystem.java
@@ -1,21 +1,23 @@
 package ca.qc.cdm.sentinelles.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static com.ctre.phoenix.motorcontrol.InvertType.FollowMaster;
 
 public class DriveSubsystem extends SubsystemBase {
     // The motors on the left side of the drive.
-    private VictorSPX leftMotor1 = new VictorSPX(0);
-    private VictorSPX leftMotor2 = new VictorSPX(1);
-    private VictorSPX rightMotor1 = new VictorSPX(2);
-    private VictorSPX rightMotor2 = new VictorSPX(2);
+    private WPI_VictorSPX leftMotor1 = new WPI_VictorSPX(1);
+    private WPI_VictorSPX leftMotor2 = new WPI_VictorSPX(2);
+    private WPI_VictorSPX rightMotor1 = new WPI_VictorSPX(3);
+    private WPI_VictorSPX rightMotor2 = new WPI_VictorSPX(4);
 
     /**
      * Creates a new DriveSubsystem.
      */
     public DriveSubsystem() {
+        super();
         // Make M2 follow exactly what M1 does
         leftMotor2.follow(leftMotor1);
         leftMotor1.setInverted(false);
@@ -25,6 +27,9 @@ public class DriveSubsystem extends SubsystemBase {
         rightMotor2.follow(rightMotor1);
         rightMotor1.setInverted(false);
         rightMotor2.setInverted(FollowMaster);
+
+        DifferentialDrive m_drive = new DifferentialDrive(leftMotor1, rightMotor2);
+
     }
 
     /**
@@ -34,6 +39,9 @@ public class DriveSubsystem extends SubsystemBase {
      * @param rot the commanded rotation
      */
     public void arcadeDrive(double fwd, double rot) {
-//        m_drive.arcadeDrive(fwd, rot);
+        m_drive.arcadeDrive(fwd, rot);
+    }
+    public void initDefaultCommand() {
+        setDefaultCommand(new DriveSubsystem());
     }
 }
