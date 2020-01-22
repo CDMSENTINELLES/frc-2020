c40001ea2b1b83ffb76d88154853d79c9d5d098a
altus34 <altus34@gmail.com>
2020-01-22 17:12:07 -0500

    WIP

--- src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
+++ src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
@@ -1,34 +1,28 @@
 package ca.qc.cdm.sentinelles;
 
 import ca.qc.cdm.sentinelles.commands.NullCommand;
-import ca.qc.cdm.sentinelles.subsystems.DriveSubsystem;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
+import ca.qc.cdm.sentinelles.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 
 public class RobotContainer {
-    private final DriveSubsystem drive = new DriveSubsystem();
-    Joystick joystick = new Joystick(1);
+    public static DriveTrain drivetrain = new DriveTrain();
+
+    XboxController joystick = new XboxController(1);
 
     private final Command autoCommand = new NullCommand();
 
     public RobotContainer() {
-        configureButtonBindings();
-
-        drive.setDefaultCommand(
+        drivetrain.setDefaultCommand(
                 // A split-stick arcade command, with forward/backward controlled by the left
                 // hand, and turning controlled by the right.
-                new RunCommand(() -> drive
-                        .arcadeDrive(joystick.getY(GenericHID.Hand.kLeft),
-                                joystick.getX(GenericHID.Hand.kRight)), drive));
+                new RunCommand(() -> {
+                    double move = joystick.getX();
+                    double rotate = joystick.getY();
+                    drivetrain.drive(move, rotate);
+                }));
 
-    }
-
-    private void configureButtonBindings() {
-//        new JoystickButton(xboxController, XboxController.Button.kBumperRight.value)
-//                .whenPressed(() -> drive.setMaxOutput(0.5))
-//                .whenReleased(() -> drive.setMaxOutput(1));
     }
 
     public Command getAutonomousCommand() {
---
+++ src/main/java/ca/qc/cdm/sentinelles/subsystems/Elevator.java
@@ -0,0 +1,8 @@
+package ca.qc.cdm.sentinelles.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+
+
+}
