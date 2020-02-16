c2eea421895ed5e049f601cf833fd23e5d4fe972
altus34 <altus34@gmail.com>
2020-02-15 21:15:52 -0500

    WIP

--- src/main/java/ca/qc/cdm/sentinelles/subsystem/drive/DriveGearbox.java
+++ src/main/java/ca/qc/cdm/sentinelles/subsystem/drive/DriveGearbox.java
@@ -1,7 +1,5 @@
 package ca.qc.cdm.sentinelles.subsystem.drive;
 
-import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
-import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
@@ -9,7 +7,6 @@ import java.util.List;
 
 import static ca.qc.cdm.sentinelles.Constants.DriveConstants.TIMEOUT_MS;
 import static com.ctre.phoenix.motorcontrol.NeutralMode.Brake;
-import static com.ctre.phoenix.motorcontrol.StatusFrame.*;
 
 public class DriveGearbox {
     private static final int SLOT_ID = 0;
@@ -33,48 +30,40 @@ public class DriveGearbox {
     }
 
     private void init() {
-        slave.follow(master);
-
-        // Configure Inversion
         master.setInverted(inverted);
         slave.setInverted(inverted);
 
-        master.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, TIMEOUT_MS);
+        slave.follow(master);
 
-        master.setStatusFramePeriod(Status_12_Feedback1, 20, TIMEOUT_MS);
-        master.setStatusFramePeriod(Status_13_Base_PIDF0, 20, TIMEOUT_MS);
-        master.setStatusFramePeriod(Status_2_Feedback0, 5, TIMEOUT_MS);
+        // TODO read the DOC
+//        master.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, TIMEOUT_MS);
+//        master.setStatusFramePeriod(Status_12_Feedback1, 20, TIMEOUT_MS);
+//        master.setStatusFramePeriod(Status_13_Base_PIDF0, 20, TIMEOUT_MS);
+//        master.setStatusFramePeriod(Status_2_Feedback0, 5, TIMEOUT_MS);
+//        master.configClosedLoopPeriod(SLOT_ID, 1, TIMEOUT_MS);
+//        master.configOpenloopRamp(0.5);
+//        master.configClosedloopRamp(0);
+//        master.setStatusFramePeriod(StatusFrameEnhanced.Status_10_Targets, TIMEOUT_MS);
 
-        master.configMotionAcceleration(2000, TIMEOUT_MS);
-        master.configMotionCruiseVelocity(2000, TIMEOUT_MS);
-
-        master.config_kP(SLOT_ID, 0.1, TIMEOUT_MS);
-        master.config_kI(SLOT_ID, 0, TIMEOUT_MS);
-        master.config_kD(SLOT_ID, 0, TIMEOUT_MS);
-        master.config_kF(SLOT_ID, 0, TIMEOUT_MS);
-        master.config_IntegralZone(SLOT_ID, 100, TIMEOUT_MS);
-        master.configClosedLoopPeakOutput(SLOT_ID, 0.50, TIMEOUT_MS);
-
-        master.configClosedLoopPeriod(SLOT_ID, 1, TIMEOUT_MS);
-
-        master.configOpenloopRamp(0.5);
-        master.configClosedloopRamp(0);
-        master.configNeutralDeadband(0.001);
-
-        master.setStatusFramePeriod(StatusFrameEnhanced.Status_10_Targets, TIMEOUT_MS);
-
-        // Configure Sensor Phase
-        master.setSensorPhase(true);
-        slave.setSensorPhase(true);
+        // TODO use with Drive to position
+//        master.configMotionAcceleration(2000, TIMEOUT_MS);
+//        master.configMotionCruiseVelocity(2000, TIMEOUT_MS);
 
         motors.forEach(motor -> {
             motor.configFactoryDefault();
 
-            motor.configPeakOutputForward(1, TIMEOUT_MS);
-            motor.configPeakOutputReverse(-1, TIMEOUT_MS);
+            motor.setSensorPhase(true);
+            motor.config_kP(SLOT_ID, 0.1, TIMEOUT_MS);
+            motor.config_kI(SLOT_ID, 0, TIMEOUT_MS);
+            motor.config_kD(SLOT_ID, 0, TIMEOUT_MS);
+            motor.config_kF(SLOT_ID, 0, TIMEOUT_MS);
+            motor.config_IntegralZone(SLOT_ID, 100, TIMEOUT_MS);
+            motor.configClosedLoopPeakOutput(SLOT_ID, 0.50, TIMEOUT_MS);
 
             motor.configNominalOutputForward(0, TIMEOUT_MS);
             motor.configNominalOutputReverse(0, TIMEOUT_MS);
+            motor.configPeakOutputForward(1, TIMEOUT_MS);
+            motor.configPeakOutputReverse(-1, TIMEOUT_MS);
             motor.configNeutralDeadband(0.001, TIMEOUT_MS);
 
             motor.enableVoltageCompensation(true);
