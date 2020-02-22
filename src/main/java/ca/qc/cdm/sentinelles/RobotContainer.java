54aca1f4a41c750fdf576cfca46e74f57c3fbc4d
altus34 <altus34@gmail.com>
2020-02-22 11:35:30 -0500

    Test command

--- src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
+++ src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
@@ -3,9 +3,16 @@ package ca.qc.cdm.sentinelles;
 import ca.qc.cdm.sentinelles.command.JoystickDrive;
 import ca.qc.cdm.sentinelles.command.NullCommand;
 import ca.qc.cdm.sentinelles.subsystem.drive.DriveSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import static edu.wpi.first.wpilibj.XboxController.Button.kA;
 
 public class RobotContainer {
+    private final XboxController xboxController = new XboxController(0);
+
     // Subsystems
     private DriveSubsystem driveTrain;
 
@@ -24,7 +31,9 @@ public class RobotContainer {
     }
 
     private void buttonBindings() {
-
+        new JoystickButton(xboxController, kA.value)
+                .whenPressed(new RunCommand(() -> System.out.println("Ka-pressed")))
+                .whenReleased(new RunCommand(() -> System.out.println("Ka-released")));
     }
 
     public Command getAutonomousCommand() {
