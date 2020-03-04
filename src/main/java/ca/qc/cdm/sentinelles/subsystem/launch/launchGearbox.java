6d3500be00d08dc64b4ada4c7681134b23a67568
ColeWorks <devatmacole@users.noreply.github.com>
2020-03-04 17:08:12 -0500

    update limelight

--- src/main/java/ca/qc/cdm/sentinelles/subsystem/launch/launchGearbox.java
+++ src/main/java/ca/qc/cdm/sentinelles/subsystem/launch/launchGearbox.java
@@ -1,14 +1,28 @@
 package ca.qc.cdm.sentinelles.subsystem.launch;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.CAN;
 
 import java.util.List;
 
 public class launchGearbox {
+    private final CANSparkMax master;
+    private final CANSparkMax slave;
+    private final boolean inverted;
+    private final List<CANSparkMax> motors;
+
     public launchGearbox(int masterId, int slaveId, boolean inverted) {
+        master = new CANSparkMax(masterId, CANSparkMaxLowLevel.MotorType.kBrushless);
+        slave = new CANSparkMax(slaveId, CANSparkMaxLowLevel.MotorType.kBrushless);
+        this.inverted = inverted;
+        motors = List.of(master, slave);
         init();
     }
     private void init() {
+        master.setInverted(inverted);
+        slave.setInverted(inverted);
+        slave.follow(master);
 
     }
 }
