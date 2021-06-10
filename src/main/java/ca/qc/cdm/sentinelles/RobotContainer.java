c82f347d4bf864c292c6064825439b34ca9f3670
Cole Dev <devc@educ.college-montreal.qc.ca>
2021-06-09 20:54:37 -0400

    Forgot to activate the subsytems and commands, oops!!!!!

--- src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
+++ src/main/java/ca/qc/cdm/sentinelles/RobotContainer.java
@@ -1,13 +1,20 @@
 package ca.qc.cdm.sentinelles;
 
+import ca.qc.cdm.sentinelles.command.ArmControl;
 import ca.qc.cdm.sentinelles.command.JoystickDrive;
 import ca.qc.cdm.sentinelles.command.NullCommand;
+import ca.qc.cdm.sentinelles.subsystem.arm.ArmSubsystem;
+import ca.qc.cdm.sentinelles.subsystem.claw.ClawSubsystem;
 import ca.qc.cdm.sentinelles.subsystem.drive.DriveSubsystem;
+import ca.qc.cdm.sentinelles.subsystem.launch.LaunchSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
     // Subsystems
     private DriveSubsystem driveTrain;
+    private LaunchSubsystem launchSubsystem;
+    private ClawSubsystem clawSubsystem;
+    private ArmSubsystem armSubsystem;
 
     // Commands
     private final Command autoCommand = new NullCommand();
@@ -17,10 +24,14 @@ public class RobotContainer {
         buttonBindings();
 
         driveTrain.setDefaultCommand(new JoystickDrive(driveTrain));
+        launchSubsystem.setDefaultCommand(new ArmControl(launchSubsystem, armSubsystem, clawSubsystem));
     }
 
     private void initSubsystem() {
         driveTrain = new DriveSubsystem();
+        launchSubsystem = new LaunchSubsystem();
+        clawSubsystem = new ClawSubsystem();
+        armSubsystem = new ArmSubsystem();
     }
 
     private void buttonBindings() {
